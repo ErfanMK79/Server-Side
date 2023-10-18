b9f37dba3a1c89d46dbd87919cb20f1527929aa6
ErfanMaleki <erfanmaleki7973@gmail.com>
2023-10-18 19:01:56 +0330

    Tamrin(1)

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Proje_1_.Models;
+
+namespace Proje_1_.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult aboutus()
+    {
+        return View();
+    }
+
+    public IActionResult contactus()
+    {
+        return View();
+    }
+
+    public IActionResult shop()
+    {
+        return View();
+    }
+
+    public IActionResult login()
+    {
+        return View();
+    }
+
+    public IActionResult product()
+    {
+        return View();
+    }
+
+    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Proje_1_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
